0f689c7e98a1616503a0263e015e6f7b0ea2aae7
rojasjhoq <jhonatanrojasquinones@gmail.com>
2021-02-18 22:19:40 +0000

    .

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -150,20 +150,17 @@ var3 = apellidos_set3.issubset(apellidos_set1)
 del 0 al 4 de la lista `apellidos_lista`.
 """
 
-apellidos_dict = dict.fromkeys(apellidos_list[0:4])
-
+apellidos_dict = dict.fromkeys(apellidos_list[0:5])
 
 """Agregar el siguiente diccionario: {"key1": 12, "key2": 24} al diccionario  
 `apellidos_dict`.
 """
-apellidos_dict['key1'] = 12
-apellidos_dict['key2'] = 24
+apellidos_dict.update({'key1':12,'key2':24} )
 
 """Asignar el valor del entero `34` a la llave "gonzalez" en el diccionario 
 `apellidos_dict`.
 """
 apellidos_dict['gonzalez'] = 34
-
 
 """Sacar el valor de la llave "gonzalez" del diccionario `apellidos_dict` y 
 guardarlo en la variable `apellido_gonzalez`.
@@ -172,10 +169,9 @@ apellido_gonzalez=apellidos_dict.pop("gonzalez")
 
 """Eliminar el último elemento del diccionario `apellidos_dict`.
 """
-
+del(apellidos_dict['key2'])
 
 """Sacar el valor de la llave "no_existe" del diccionario `apellidos_dict` y 
 guardarlo en la variable `apellido_none`.
 """
-apellido_none = apellidos_dict.get('GARCIA','None')
-print(apellido_none)
+apellido_none = apellidos_dict.get("no_existe")
